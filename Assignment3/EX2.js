578b7c08f74b7561f73f91bce227d379c5123d02
Altlalka Daher <daheral@post.bgu.ac.il>
2024-09-18 01:46:37 +0300

    add a new funtion of ex 6.2

--- Assignment3/EX2.js
+++ Assignment3/EX2.js
@@ -8,3 +8,16 @@ function accum(word){
     return ans
 }
 
+console.log("EX6.2");
+function countDuplicates(strs){
+    let arr = strs.split("");
+    let chars = []
+    let repets = arr.reduce((acc, char) => {
+        acc[char] =  acc[char] ? acc[char] + 1 : 1;
+        return acc;
+    }, {}); 
+    let ans = Object.entries(repets).filter(([keys, value]) => value > 1)
+    return ans.length;
+}
+
+
